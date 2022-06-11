ed88460b08cfc818db6a13c60a58eff505dfea5b
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-06-11 22:50:44 +0400

    uploaded second time

---
+++ NegativeToPositive/NegativeToPositive/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace NegativeToPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mixNums = {0, -7, 1, 7, -23};
+            MakePositive(mixNums);
+        }
+
+        private static int[] MakePositive(int[] mixNums)
+        {
+            for (var i = 0; i < mixNums.Length; i++)
+            {
+                if (mixNums[i] < 0)
+                {
+                    mixNums[i] *= -1;
+                }
+                Write($"{mixNums[i]}; ");
+            }
+            return mixNums;
+        }
+    }
+}
